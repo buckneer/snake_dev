694a0043895a8c6255e078a06cffb04e2386dfb9
Miftari Simel <miftarisimel@gmail.com>
2023-02-06 10:32:03 +0100

    Code cleanup

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from snake import snake
 import turtle
+from time import sleep
 
 screen = turtle.Screen()
 
 s = snake.Snake(screen)
+
+s.write_word("Zdravo!")
+sleep(1)
+s.delete_every_letter()
 
 
 screen.mainloop()
